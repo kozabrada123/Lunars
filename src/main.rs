505174177a5ba826382e8c47ba0bb90ea43c1f16
kozabrada123 <natantriestoedit@gmail.com>
2022-08-08 16:25:19 +0200

    Misc log features ( ͡° ͜ʖ ͡°)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-// Main file! Hosts server that connects to database.
+// Main file: Hosts server that connects to database.
 // Make sure to first configure .env and your json file with key hashes.
+
 // -----------------------
 
 // Imports
@@ -15,8 +16,8 @@ mod db;
 
 use crate::db::{Match, Player};
 use dotenv::dotenv;
-use log::{debug, error, warn};
-use flexi_logger::{FileSpec, Logger, WriteMode};
+use log::{debug, error, warn, info};
+use flexi_logger::{FileSpec, Logger, WriteMode, Duplicate, colored_default_format};
 use nickel::status::StatusCode;
 use nickel::{HttpRouter, JsonBody, Nickel};
 use serde::{Deserialize, Serialize};
@@ -76,8 +77,13 @@ fn main() {
     // Init logger
     let _logger = Logger::try_with_str("info, lunars=trace").unwrap()
         .log_to_file(FileSpec::default())
+        .duplicate_to_stdout(Duplicate::All)
+        .format_for_stdout(colored_default_format)
         .write_mode(WriteMode::BufferAndFlush)
         .start().unwrap();
+
+    // Init beautiful art into the log
+    log_logo();
 
     // Make a nickel server
     let mut server = Nickel::new();
@@ -774,6 +780,54 @@ fn process_game_test(data: GameStruct, player_a: &Player, player_b: &Player) {
     );
 
     dbcon.conn.close().unwrap();
+}
+
+
+// 100% required feature ( ͡° ͜ʖ ͡°)
+// There is a better way to do this, like using a text file or converting but for some reason I dont want to do that
+fn log_logo() {
+    info!("");
+    info!("                   &##&                        5?& JJ                       &#B#&                   ");
+    info!("                &#BB#                        &J~~7?~~?&                       &BBB#&                ");
+    info!("           && &BBBB&                   #GPYJ?!~~~~~~~~!?JYPG#&                  #BBBB&&#&           ");
+    info!("         #BB&#BBB#                &B5?!~~~~^^~~~~~~~~~~^^~~~~!?5B&               &#BBB&&BB&         ");
+    info!("        #BB&#BB#&#&             GJ!~~~~~~~!7?J?~~~~~~7J?7!~~~~~~~!JG&            #&&#BB #BB#        ");
+    info!("       #BB# #&&#B#           &57~~~~~~7YP#&    ?~~~~~#   &#GY?!~~~~~!Y#          &BB#&#& BBB&#&     ");
+    info!("    ## BBB &#BBB#          &Y!~~~~~?P#         ?~~~~~#        &GJ!~~~~~Y#          #BBB#&#BB#&B&    ");
+    info!("   #B&&BB##BBB#           P!~~~~~J#            ?~~~~~#           &5!~~~~~5           #BBBBBB# BB&   ");
+    info!("  &BB#&BBBB#&#          &?~~~~~J#              ?~~~~~#             &5!~~~~?#         ##&#BBB# BBB   ");
+    info!("  #BB#&BB&&#B#         #7~~~~!B                J~~~~~&               #?~~~~!#        &BB&&&B# BBB&  ");
+    info!("  #BBB&&&#BBB         &7~~~~7#              &GP!~~~~~YPB               J~~~~!#        &BBB#& &BBB#  ");
+    info!("  #BBB&&BBB#          J~~~~!&               5^~~~~~~~~~~#               J~~~~?          #BBB##BBB&& ");
+    info!("#& BBBBBBB&          G~~~~~B            &##G!~~~~~~~~~~~?B#&            &!~~~~P         &&BBBBBB# B#");
+    info!("B# #BBBB# ##       #G7~~~~!&           G!~~~~~~~~~~~~~~~~~~~?            J~~~~7G#       B& #BBBB &BB");
+    info!("BB& BBB& #B#     G?~~~~~~~~7???????????!~~~~~~~~~~~~~~~~~~~~~7??????????7~~~~~~~~?P     #B# &BB& BBB");
+    info!("BBB& B# #BB&     &G?~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~?G&     &BBB &# #BB#");
+    info!("#BBB&  BBB#      G?~~~~~~~~!77777777777~~~~~~~~~~~~~~~~~~~~~~!7777777777!~~~~~~~~7P      #BBB  #BBB&");
+    info!(" BBBB #BBB         #P7~~~~!#           5~~~~~~~~~~~~~~~~~~~~7&           ?~~~~!5B&        BBB##BBB# ");
+    info!("  BBB#BBB&&#         G~~~~~B            BGPY~~~~~~~~~~~~!5PG#           &7~~~~P        &# &BBBBBB# &");
+    info!("B& #BBBBB #B&         ?~~~~7&               Y^~~~~~~~~~~B               J~~~~7         #B& #BBBB& #B");
+    info!("BB& &BBB& BB#         #!~~~~?&              &5Y!~~~~~JYP               Y~~~~!#         BB# #BB# &BB#");
+    info!("&BBB&&#B& BBB          #!~~~~7B                ?~~~~~&               &J~~~~!B         &BBB &#&&#BB# ");
+    info!(" &BBBB&& &BBB&          #?~~~~~Y&              ?~~~~~#              P!~~~~7#        & &BBB  &#BBB#  ");
+    info!("  &#BBBB&&BBB&&B&         5~~~~~!Y#            ?~~~~~#           &P7~~~~~Y        &BB #BBB&#BBBB&   ");
+    info!("    &#BBBBBBB# #B#         #J~~~~~~JG&         ?~~~~~#        &BY!~~~~~J#        &BB# BBBBBBB#&     ");
+    info!("    #&&#BBBBBB &BB#          #Y!~~~~~!?5G#&    ?~~~~~&    &B5?!~~~~~!Y#         &BBB  BBBBB#& ##    ");
+    info!("    &B#&&&&#BB# BBB#           &GJ!~~~~~~~!7JYJ!~~~~~?JJ?7~~~~~~~!?P&           BBB# #B#&&&&#B#     ");
+    info!("     &BBB##&&&# &BBB# &           &GY?!~~~~^^~~~~~~~~~~^^~~~~!?YG&          && #BBB  &&&#BBBB&      ");
+    info!("       &#BBBB##& &BBB&&B#&            &#G5J?7!~~~~~~~~!7?J5GB&            &BB&#BBB&&##BBBBB&        ");
+    info!("         &##BBBBB#BBBB&&BBB&                 &?~~77~~?#                 #BBB&&BBBBBBBBB##&          ");
+    info!("            &&&##BBBBBB&&BBBB&                 Y?&&?Y                &#BBB#&#BBBBB##&&&&&           ");
+    info!("            &##&&&&&&###& &#BBB#&                                  &#BBB#& &#&&&&&&##BB&            ");
+    info!("              #BBBBBBBBBBB##BBBBBBB##&  &&&&#&&&    &&###&&&   ##BBBBBBBBBBBBBBBBBBB#&              ");
+    info!("                 &&###BBBBBB##########BBBBB###BBB##BBB####BBBB####&&############&&                  ");
+    info!("                    &&&&&   &&&##BBBBBB#&&&##B#&&   &#B##& &#BBBBBB###&&&&&&&&#&                    ");
+    info!("                     &##BBBBBBBBBBBB#& &#BBB&          &BBB#&&&#BBBBBBBBBBBB##&                     ");
+    info!("                         &&#####&&&  &#BB#&              &#BBB&   &&&&&&&&                          ");
+    info!("                                     &B#&                  &#B#                                     ");
+    info!("");
+    info!("{}", format!("{}: v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")));
+    info!("");
 }
 
 // Tests!
